91dcd6c0590343ca31399dcb7b4ec066a885b6cb
krzyhooarrow <strzala.krzysztof@interia.pl>
2020-01-13 22:59:34 +0100

    loopy

--- grammar/grammar.py
+++ grammar/grammar.py
@@ -1,17 +1,20 @@
+import os
+
 import ply.yacc as yacc
 from pip._vendor.distlib.compat import raw_input
 
 from lekser.lex import Lexer
 
 tokens = Lexer().tokens
-
+replacement = ['JZERO','JNEG','JPOS','JUMP']
 variables = {}  # contains var name and address
 arrays = {}  # contains array name and its address
 initialized_variables = set()  # contains already initialized vars
-current_instruction = 1  # memory counter
-register_number=1
-
-
+current_instruction = 0
+register_number = 1  # memory counter
+stack = []
+valueJZERO = 0
+valueJUMP = 0
 ########################################################################################################################
 # Declaring 10 temporary registers
 
@@ -21,12 +24,12 @@ register_number=1
 ########################################################################################################################
 def p_program_with_libs(p):
     'program : DECLARE declarations BEGIN commands END'
-    p[0] = str(p[4]) + '\nHALT'
+    p[0] = str(p[4][0]) + '\nHALT'
 
 
 def p_program_without_libs(p):
     'program : BEGIN commands END'
-    p[0] = p[2] + '\nHALT'
+    p[0] = p[2][0] + '\nHALT'
 
 
 ########################################################################################################################
@@ -40,11 +43,9 @@ def p_declarations_next_variable(p):
     new_variable(p[3], str(p.lineno(3)))
 
 
-
 def p_declarations_next_array(p):
     'declarations : declarations COMMA pidentifier LEFT_BRACKET NUM COLON NUM RIGHT_BRACKET'
     new_array(p[3], p[5], p[7], str(p.lineno(3)))
-
 
 
 def p_declarations_variable(p):
@@ -52,11 +53,9 @@ def p_declarations_variable(p):
     new_variable(p[1], str(p.lineno(1)))
 
 
-
 def p_declarations_array(p):
     'declarations : pidentifier LEFT_BRACKET NUM COLON NUM RIGHT_BRACKET'
     new_array(p[1], p[3], p[5], str(p.lineno(1)))
-
 
 
 ########################################################################################################################
@@ -66,12 +65,12 @@ def p_declarations_array(p):
 ########################################################################################################################
 def p_commands_multi(p):
     'commands : commands command'
-    p[0] = str(p[1]) + str(p[2])
-
+    # print(current_instruction)
+    p[0] = (str(p[1][0]) + str(p[2][0]),p[1][1]+p[2][1])
 
 def p_commands_single(p):
     'commands : command'
-    p[0] = str(p[1])
+    p[0] = p[1]
 
 
 ########################################################################################################################
@@ -87,36 +86,44 @@ def p_commands_single(p):
 # 20 | WRITE value ;
 ########################################################################################################################
 
-
+### KOMENDY ZWRACAJA ICH KOSZT
 def p_command_assign(p):
     'command : identifier ASSIGN expression SEMICOLON'
-    variable_check(p[1][1],'0')
-    p[0] = str(assign_value_to_variable(p[3],p[1])) + f'\nSTORE {variables[p[1][1]]}'
+    variable_check(p[1][1], '0')
+    initialized_variables.add(p[1][1])
+    global current_instruction
+    ASSIGMENT = assign_value_to_variable(p[3], p[1])
+    current_instruction += ASSIGMENT[1]+1
+    #
+    # koszt 0 bo zostal dodany juz w current instr  ASSIGMENT[1]+1
+    p[0] = str(ASSIGMENT[0]) + f'\nSTORE {variables[p[1][1]]}',ASSIGMENT[1]+1
+    # cost = ASSIGMENT[1] + 1
+    # stack.append((cost,p[0]))
+
 
 
 def p_command_if_else(p):
     'command : IF condition THEN commands ELSE commands ENDIF'
-    p[0] = 'IF', p[2], 'THEN', p[4], 'ELSE', p[6], 'ENDIF'
-
+    p[0] = (str(p[2][0]) + f'\nJZERO {p[6][1]+2}' + str(p[6][0]) +f'\nJUMP {p[4][1]+1}'+str(p[4][0]),p[2][1]+p[4][1]+p[6][1]+2)
 
 def p_command_if(p):
     'command : IF condition THEN commands ENDIF'
-    p[0] = ('IF', p[2], 'THEN', p[4], 'ENDIF')
-
+    p[0] = (str(p[2][0]) + f'\nJZERO 2\nJUMP {p[4][1]+1}' + str(p[4][0]), p[2][1] + p[4][1] + 2)
 
 def p_command_while_do(p):
     'command : WHILE condition DO commands ENDWHILE'
-    p[0] = ('WHILE', p[2], 'DO', p[4], 'ENDWHILE')
-
+    p[0] = (str(p[2][0]) + f'\nJZERO 2\nJUMP {p[4][1]+2}' + str(p[4][0]) + f'\nJUMP {-(p[4][1]+p[2][1]+2)}', p[2][1] + p[4][1] + 3)
+                                                                            #cofamy sie o roznice na początek condition!!
 
 def p_command_do_while(p):
     'command : DO commands WHILE condition ENDDO'
-    p[0] = ('DO', p[2], 'WHILE', p[4], 'ENDDO')
+    p[0] = (p[2][0] + p[4][0] + f'\nJPOS 3\nJNEG 2\nJUMP {-(p[4][1]+p[2][1]+2)}', p[2][1] + p[4][1] + 3)
+        # ('DO', p[2][0], 'WHILE', p[4], 'ENDDO')
 
 
 def p_command_from_upto(p):
     'command : FOR pidentifier FROM value TO value DO commands ENDFOR'
-    p[0] = ('FOR', p[2], 'FROM', p[4], 'TO', p[6], 'DO', p[8], 'ENDFOR')
+    p[0] = ('FOR', p[2], 'FROM', p[4], 'TO', p[6], 'DO', p[8][0], 'ENDFOR')
 
 
 def p_command_from_downto(p):
@@ -126,15 +133,14 @@ def p_command_from_downto(p):
 
 def p_command_read(p):
     'command : READ identifier SEMICOLON'
-    variable_check(p[2][1],'0')
-    p[0] = f'\nGET\nSTORE {variables[p[2][1]]}'  #?? jak to ma wygladac w sumie
-        # ('READ', p[2], 'SEMICOLON')
+    variable_check(p[2][1], '0')
+    p[0] = f'\nGET\nSTORE {variables[p[2][1]]}', 2
 
 
 def p_command_write(p):
     'command : WRITE value SEMICOLON'
-    p[0] = assign_value_to_variable(p[2],p[2]) + '\nPUT'
-    # p[0] = ('WRITE', p[2], 'SEMICOLON')
+    ASSIGMENT = assign_value_to_variable(p[2], p[2])
+    p[0] = ASSIGMENT[0] + '\nPUT',ASSIGMENT[1] + 1
 
 
 ########################################################################################################################
@@ -154,20 +160,20 @@ def p_expression_value(p):
 
 def p_expression_plus(p):
     'expression : value PLUS value'
-    p[0] = assign_value_to_variable(p[1],p[1]) + f'\nSTORE 1' + \
-           assign_value_to_variable(p[3],p[3]) + '\nADD 1'
-    # print(p[0])
-    # p[0]= p[1]
-        # ('variable','r0')
-        # (p[1],'plus',p[3])
-        # assign_value_to_variable(p[1])
-
-                                    # 'PLUS', p[3])
+    ASSIGMENT1 = assign_value_to_variable(p[1], p[1])
+    ASSIGMENT2 = assign_value_to_variable(p[3], p[3])
+    p[0] = ASSIGMENT1[0] + f'\nSTORE 1' + \
+          ASSIGMENT2[0] + '\nADD 1',ASSIGMENT1[1]+ASSIGMENT2[1]+2
 
 
 def p_expression_minus(p):
     'expression : value MINUS value'
-    p[0] = (p[1], 'MINUS', p[3])
+    ASSIGMENT1 = assign_value_to_variable(p[1], p[1])
+    ASSIGMENT2 = assign_value_to_variable(p[3], p[3])
+
+    p[0] = ASSIGMENT2[0] + f'\nSTORE 1' + \
+           ASSIGMENT1[0] + '\nSUB 1', ASSIGMENT1[1] + ASSIGMENT2[1] + 2
+
 
 
 def p_expression_times(p):
@@ -194,8 +200,17 @@ def p_expression_mod(p):
 ########################################################################################################################
 def p_condition_equals(p):
     'condition : value EQ value'
-    p[0] = (p[1], 'EQ', p[3])
 
+    ASSIGMENT1 = assign_value_to_variable(p[1], p[1])
+    ASSIGMENT2 = assign_value_to_variable(p[3], p[3])
+
+    p[0] = ASSIGMENT1[0] + f'\nSTORE 1' + \
+           ASSIGMENT2[0] + '\nSUB 1', ASSIGMENT1[1] + ASSIGMENT2[1] + 2
+
+    # w p0 jest var1-var2
+
+    cost = ASSIGMENT1[1] + ASSIGMENT2[1] + 2,0
+    # stack.append((cost,p[0]))
 
 def p_condition_not_equals(p):
     'condition : value NEQ value'
@@ -207,20 +222,37 @@ def p_condition_less(p):
     p[0] = (p[1], 'LE', p[3])
 
 
+
+
 def p_condition_greater(p):
     'condition : value GE value'
+
     p[0] = (p[1], 'GE', p[3])
+
 
 
 def p_condition_less_or_equal(p):
     'condition : value LEQ value'
-    p[0] = (p[1], 'LEQ', p[3])
+    ASSIGMENT1 = assign_value_to_variable(p[1], p[1])
+    ASSIGMENT2 = assign_value_to_variable(p[3], p[3])
+
+    p[0] = ASSIGMENT1[0] + f'\nSTORE 1' + \
+           ASSIGMENT2[0] + f'\nSUB 1\nJNEG 2\nSUB 0', ASSIGMENT1[1] + ASSIGMENT2[1] + 4
+        #{JPOS + 4}                 #robi 1 krok do przodu jezeli to nie jest prawdą
+    # w p0 jest var2-var1. A nastepnie sprawdzenie JPOS czy
+
 
 
 def p_condition_greater_or_equal(p):
     'condition : value GEQ value'
-    p[0] = (p[1], 'GEQ', p[3])
 
+    ASSIGMENT1 = assign_value_to_variable(p[1], p[1])
+    ASSIGMENT2 = assign_value_to_variable(p[3], p[3])
+
+    p[0] = ASSIGMENT1[0] + f'\nSTORE 1' + \
+           ASSIGMENT2[0] + f'\nSUB 1\nJPOS 2\nSUB 0', ASSIGMENT1[1] + ASSIGMENT2[1] + 4
+        #{JPOS + 4}                 #robi 1 krok do przodu jezeli to nie jest prawdą
+    # w p0 jest var2-var1. A nastepnie sprawdzenie JPOS czy
 
 ########################################################################################################################
 # 36 value -> num
@@ -254,7 +286,6 @@ def p_identifier_pidentifier_pidentifier(p):
 
 def p_identifier_pidentifier_num(p):
     'identifier : pidentifier LEFT_BRACKET NUM RIGHT_BRACKET'
-    # p[0] = (p[1], 'LEFT_BRACKET', p[3], 'RIGHT_BRACKET')
     p[0] = ("array", p[1], ("CONSTANT", p[3]))
 
 
@@ -288,25 +319,25 @@ def initialization_check(id, lineno):
 
 # Function creates new variable and stores its address in dict variables.
 def new_variable(var_name, line_number):
-    global current_instruction
+    global register_number
     if var_name in variables:
         raise Exception("Duplicated variable name at line: " + line_number)
     else:
-        variables[var_name] = current_instruction
+        variables[var_name] = register_number
         # initialized_variables.add(var_name)
-        current_instruction += 1
+        register_number += 1
 
 
 def new_array(array_name, begin, end, line_number):
-    global current_instruction
+    global register_number
     if begin > end:
         raise Exception('Error in array range')
     if array_name in arrays:
         raise Exception("Duplicated array name at line: " + line_number)
     else:
-        arrays[array_name] = [current_instruction, begin, end]
+        arrays[array_name] = [register_number, begin, end]
         # initialized_variables.add(array_name)      # initialized_variables.add(array_name)
-        current_instruction += begin - end + 1
+        register_number += begin - end + 1
 
 
 def create_temporary_registers(length):
@@ -317,9 +348,11 @@ def create_temporary_registers(length):
 
 ########################################################################################################################
 
-def assign_value_to_variable(value,assigned=None):
+
+# returns string, cost of operations
+def assign_value_to_variable(value, assigned=None):
     if value[0] == 'variable':
-        initialization_check(value[1],'0')
+        initialization_check(value[1], '0')
         initialized_variables.add(assigned[1])
         return store_variable(value[1])
     elif value[0] == 'CONSTANT':
@@ -329,32 +362,36 @@ def assign_value_to_variable(value,assigned=None):
         # if id[2][0] == 'CONSTANT':
         #     return arrays[id[1]][0] + id[2][1] - arrays[id[1]][1]
         # if id[2][0] == 'variable':
-            pass  # to be done
+        pass  # to be done
     else:
         return value
 
 
 def store_variable(variable):
-   return f'\nLOAD {variables[variable]}'
+    return f'\nLOAD {variables[variable]}',1
 
 
 def store_constant(value):
-    value=value[1]
+    counter = 0
+    value = value[1]
     STORE = '\nSUB 0'
     if value > 0:
         sign = True
     else:
         sign = False
+    counter += 1
     binary_value = str(bin(abs(value)))[2:]
 
     for k in range(len(binary_value)):
         STORE += '\nADD 0'
+        counter += 1
         if binary_value[k] == '1':
+            counter += 1
             if sign:
                 STORE += '\nINC'
             else:
                 STORE += '\nDEC'
-    return STORE
+    return STORE, counter
 
 
 #######################################################################################################################
@@ -369,13 +406,42 @@ parser = yacc.yacc(
 file = open(input, "r")
 try:
     parsed = parser.parse(file.read(), tracking=True)
+    if parsed == '\n':
+        parsed = ''
+    parsed = parsed.strip()
+    i = 0
+    commands = parsed.split('\n')
+    while i < len(commands):
+        if commands[i].split()[0] in replacement:
+            # print(int(commands[i].split()[1]) + i)
+            commands[i]= commands[i].split()[0] +' ' + str(int(commands[i].split()[1]) + i)
+            # commands[i].replace(commands[i].split()[1],str(int(commands[i].split()[1]) + i))
+            # commands[i].split()[1]= str(int(commands[i].split()[1]) + i)
+
+            # print(commands[i])
+            # print(i)
+        i+=1
+    PROGRAM = ''
+    for command in commands:
+        PROGRAM += command + '\n'
+    print(PROGRAM)
+
+        # if lines.split()[0] in replacement:
+        #     value = parsed.split()[1]
+        #     parsed.replace(value,'value+5')
+    # parsed.replace()
+    # [int(s) for s in parsed.split() if s.isdigit()]
+
 except Exception as e:
     print(e)
     exit()
 fw = open(output, "w")
-print(f'{parsed}')
-print(variables)
-print(arrays)
-print(initialized_variables)
-fw.write(f'{parsed}')
+# print(f'{parsed}')
+# print(parsed.strip())
+# print(variables)
+# print(arrays)
+# print(initialized_variables)
+# fw.write(f'{parsed.strip()}')
+fw.write(PROGRAM)
+#       /home/krzyhoo/Desktop/Compiler/virtual_machine/maszyna-wirtualna /home/krzyhoo/Desktop/Compiler/grammar/output.txt
 
