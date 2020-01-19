72c76c20474c58aac9aed3ca003572c92ce0e04c
krzyhooarrow <strzala.krzysztof@interia.pl>
2020-01-19 11:29:48 +0100

    all tests for now passed

--- grammar/grammar.py
+++ grammar/grammar.py
@@ -255,6 +255,9 @@ def p_expression_times(p):
 
             , PREPARE_FOR_MULTIPLICATION[1] + ASSIGMENT1[1] + ASSIGMENT2[1] + LOOP_DIST + end_if_equals_0(0)[1] + 9+negate_value_if_0()[1])
 
+
+
+
 ########################################################################################################################
 # powers are stored in 9
 # adds in 8
@@ -304,8 +307,7 @@ def p_expression_div(p):
     ASSIGMENT1 = assign_value_to_variable(p[1], p[1])
     ASSIGMENT2 = assign_value_to_variable(p[3], p[3])
 
-    TOTAL_DISTANCE = make_floor_if_neg()[1]+change_value_if_sign_flag_is_on()[1] + divide()[1] + 6
-        # zamiast + 2
+    TOTAL_DISTANCE = change_value_if_sign_flag_is_on()[1] + divide()[1] + 2
 
     LOOP_DISTANCE_FROM_ASSIGMENT_2_TO_END = TOTAL_DISTANCE + assert_bigger_value_division(1, 1)[1]
     LOOP_DISTANCE_FROM_ASSIGMENT_1_TO_END = LOOP_DISTANCE_FROM_ASSIGMENT_2_TO_END + assert_0_division(1)[1] + check_sign_of_value()[1]+\
@@ -328,12 +330,10 @@ def p_expression_div(p):
            f'\nJUMP 4\nSUB 0\nINC\nSTORE 8' \
            \
            f'{change_value_if_sign_flag_is_on()[0]}' \
-           f'{make_floor_if_neg()[0]}' \
            f'\nLOAD 8' \
         , ASSIGMENT2[1]+ASSIGMENT1[1]+2*change_sign_flag()[1]+9+\
-           check_sign_of_value()[1]+assert_0_division(0)[1]+assert_bigger_value_division(1,1)[1]\
-           +divide()[1]+change_value_if_sign_flag_is_on()[1]+\
-           load_registers_for_division()[1] + make_floor_if_neg()[1]
+           check_sign_of_value()[1]+assert_0_division(0)[1]+assert_bigger_value_division(1,1)[1]+divide()[1]+change_value_if_sign_flag_is_on()[1]+load_registers_for_division()[1]
+
 
 ########################################################################################################################
 # w 1 flaga znaku = 0 gdy są tego samego znaku
@@ -381,7 +381,7 @@ def check_if_power_equals_1(cond_dist,end_dist):
     return (f'\nJPOS {cond_dist+4}\nJZERO {cond_dist+3}\nLOAD 9\nJZERO {end_dist}',4)
 
 def make_floor_if_neg():
-    return ('\nLOAD 5\nADD 7\nJZERO 7\nLOAD 1\nJPOS 5\nJNEG 4\nLOAD 8\nDEC\nSTORE 8',9)
+    return ('\nLOAD 5\nJZERO 7\nLOAD 1\nJPOS 5\nJNEG 4\nLOAD 8\nDEC\nSTORE 8',8)
 
 
 def decrease_power():
@@ -434,12 +434,10 @@ def p_expression_mod(p):
         check_returned_sign()[0] + \
         '\nLOAD 5'\
         ,\
-        4+DISTANCE_TO_END+check_if_divider_equals_0_or_1(1)[1]\
-        +ASSIGMENT2[1]+ASSIGMENT2[1]\
-        + clear_sign_flag_and_set_power_to_0()[1]\
-        +2*change_sign_flag()[1]
-
-
+        clear_sign_flag_and_set_power_to_0()[1]+ASSIGMENT1[1]+2+change_sign_flag()[1]*2+\
+        ASSIGMENT2[1]+2+check_if_divider_equals_0_or_1(1)[1]+check_sign_of_value()[1]+\
+        find_modulus()[1]+if_equals_0_end(0)[1]+change_modulo_if_flag_on()[1]+check_returned_sign()[1]+1
+     
 ########################################################################################################################
 
 def check_returned_sign():
@@ -473,7 +471,8 @@ def find_modulus():
             f'{update_value()[0]}'
             f'{reset_power()[0]}'
             f'\nJUMP {-WHOLE_DIST}'
-            ,WHOLE_DIST)
+            ,
+            7+increase_power()[1]+decrease_power()[1]+update_value()[1]+reset_power()[1])
 
 
 def reset_power():
